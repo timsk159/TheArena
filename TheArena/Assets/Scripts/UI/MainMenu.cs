ca425b521de11bd5b4b5f0023244a7b57f763b3a
timsk159 <timsk951@hotmail.co.uk>
2014-11-26 13:02:18 +0000

    Initial folder structure, frontmenu, test scene and empty scripts

---
+++ TheArena/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+	#region Button Responses
+
+	public void PlayButtonPressed()
+	{
+		Application.LoadLevel("TestMap");
+	}
+
+	public void ExitButtonPressed()
+	{
+		Application.Quit();
+	}
+
+	#endregion
+}
