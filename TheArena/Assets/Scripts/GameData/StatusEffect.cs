fce5da32c858e5c9ad1819c8ad9a1e1e717de582
timsk159 <timsk951@hotmail.co.uk>
2014-11-27 22:28:25 +0000

    Added comment to status effect script

--- TheArena/Assets/Scripts/GameData/StatusEffect.cs
+++ TheArena/Assets/Scripts/GameData/StatusEffect.cs
@@ -4,7 +4,15 @@ using System.Collections;
 
 //When creating new status effects, just inherit from this class.
 public class StatusEffect : ScriptableObject
-{	
+{
+	/* Make sure to add one of these when inheriting:
+	[MenuItem("Assets/Create/Custom/StatusEffects/EffectName")]
+	public static void CreateAsset()
+	{
+		ScriptableObjectUtility.CreateAsset<ClassName>();
+	}
+	 */
+
 	public virtual void Apply(Character character)
 	{
 
