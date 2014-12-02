cf3074a7d77d25fb48b4b103b3e537a018d9be95
Roiku <stathap@gmail.com>
2014-12-02 14:10:24 +0000

    Added Input Manager + Debugger and Arena Asset to the main scene

---
+++ TheArena/Assets/Scripts/Character/InputManager.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class InputManager : MonoBehaviour
+{
+    //-------------------------------------------------
+
+    public enum InputActions
+    { 
+        UP,
+        LEFT,
+        DOWN,
+        RIGHT,
+        PRIMARY_ATTACK,   // Default Mouse Input
+        SECONDARY_ATTACK, // Default Mouse Input
+        BLOCK,
+        DODGE,
+        SWAP_WEAPON,
+        ABILITY_1,
+        ABILITY_2,
+        ABILITY_3,
+        ABILITY_4,
+        ABILITY_5,
+        CONSUMABLE_1,
+        CONSUMABLE_2
+    };
+
+    private Dictionary<InputActions, KeyCode> KeyMapping;
+    private Dictionary<InputActions, int> MouseMapping;
+
+    //-------------------------------------------------
+
+    private void Awake()
+    {
+        KeyMapping = new Dictionary<InputActions, KeyCode>();
+        MouseMapping = new Dictionary<InputActions, int>();
+        SetDefaultKeybindings();
+    }
+
+    //-------------------------------------------------
+
+    #region Key Mapping Functions
+
+    public void SetDefaultKeybindings()
+    {
+        // Keyboard
+        KeyMapping.Add(InputActions.UP, KeyCode.W);
+        KeyMapping.Add(InputActions.LEFT, KeyCode.A);
+        KeyMapping.Add(InputActions.DOWN, KeyCode.S);
+        KeyMapping.Add(InputActions.RIGHT, KeyCode.D);
+        KeyMapping.Add(InputActions.BLOCK, KeyCode.LeftShift);
+        KeyMapping.Add(InputActions.DODGE, KeyCode.Space);
+        KeyMapping.Add(InputActions.SWAP_WEAPON, KeyCode.Tab);
+        KeyMapping.Add(InputActions.ABILITY_1, KeyCode.Alpha1);
+        KeyMapping.Add(InputActions.ABILITY_2, KeyCode.Alpha2);
+        KeyMapping.Add(InputActions.ABILITY_3, KeyCode.Alpha3);
+        KeyMapping.Add(InputActions.ABILITY_4, KeyCode.Alpha4);
+        KeyMapping.Add(InputActions.ABILITY_5, KeyCode.R);
+        KeyMapping.Add(InputActions.CONSUMABLE_1, KeyCode.Q);
+        KeyMapping.Add(InputActions.CONSUMABLE_2, KeyCode.E);
+
+        // Mouse
+        MouseMapping.Add(InputActions.PRIMARY_ATTACK, 0);
+        MouseMapping.Add(InputActions.SECONDARY_ATTACK, 1);
+    }
+
+    public void SetPreferenceKeybindings()
+    {
+        // TO-DO: load player pref and load the specified keybindings
+    }
+
+    public void SetKeybinding(KeyCode key, InputActions forAction)
+    {
+
+    }
+
+    public void SetKeybinding(string key, InputActions forAction)
+    {
+
+    }
+
+    public void SetKeybinding(int mouse, InputActions forAction)
+    {
+
+    }
+
+    #endregion
+
+    //-------------------------------------------------
+
+    #region Input Functions
+
+    public bool GetInput(InputActions forAction, bool continuous = false)
+    {
+        KeyCode keyboardPress = GetKeyboardInput(forAction);
+        if (keyboardPress != KeyCode.None)
+        {
+            if (continuous)
+            {
+                return Input.GetKey(keyboardPress);
+            }
+            else
+            {
+                return Input.GetKeyDown(keyboardPress);
+            }
+        }
+        int mousePress = GetMouseInput(forAction);
+        if (mousePress != 3)
+        {
+            if (continuous)
+            {
+                return Input.GetMouseButton(mousePress);
+            }
+            else
+            {
+                return Input.GetMouseButtonDown(mousePress);
+            }
+        }
+        return false;
+    }
+
+    private KeyCode GetKeyboardInput(InputActions forAction)
+    {
+        if (KeyMapping.ContainsKey(forAction))
+        {
+            return KeyMapping[forAction];
+        }
+        else
+        {
+            return KeyCode.None;
+        }
+    }
+
+    private int GetMouseInput(InputActions forAction)
+    {
+        if (MouseMapping.ContainsKey(forAction))
+        {
+            return MouseMapping[forAction];
+        }
+        else
+        {
+            return 3; // Non - Assigned mouse button
+        }
+    }
+
+    #endregion
+
+    //-------------------------------------------------
+}
---
+++ TheArena/Assets/Scripts/Character/InputManagerDebugger.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManagerDebugger : MonoBehaviour
+{
+    public InputManager inputManager;
+
+    void Update()
+    {
+        if (inputManager.GetInput(InputManager.InputActions.PRIMARY_ATTACK))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [PRIMARY_ATTACK]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.SECONDARY_ATTACK))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [SECONDARY_ATTACK]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.UP, true))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [UP]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.LEFT, true))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [LEFT]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.DOWN, true))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [DOWN]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.RIGHT, true))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [RIGHT]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.BLOCK, true))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [BLOCK]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.DODGE))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [DODGE]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.SWAP_WEAPON))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [SWAP_WEAPON]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.ABILITY_1))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [ABILITY_1]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.ABILITY_2))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [ABILITY_2]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.ABILITY_3))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [ABILITY_3]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.ABILITY_4))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [ABILITY_4]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.ABILITY_5))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [ABILITY_5]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.CONSUMABLE_1))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [CONSUMABLE_1]");
+        }
+
+        if (inputManager.GetInput(InputManager.InputActions.CONSUMABLE_2))
+        {
+            Debug.Log("<INPUT MANAGER>: Performing [CONSUMABLE_2]");
+        }
+    }
+}
