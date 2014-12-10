69b0289767df8a061ae0bda6ed392a22f2fd3c65
timsk159 <timsk951@hotmail.co.uk>
2014-12-10 15:19:54 +0000

    Added save system and global events class

--- TheArena/Assets/Scripts/Character/CharacterData.cs
+++ TheArena/Assets/Scripts/Character/CharacterData.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
-public class CharacterData : MonoBehaviour {
+[Serializable()]
+public class CharacterData : ISerializable
+{
+	public string name;
+	public int level;
+	public int currentXP;
 
-	// Use this for initialization
-	void Start () {
-	
+	public CharacterData(SerializationInfo info, StreamingContext ctxt)
+	{
+		name = (string)info.GetValue("CharacterName", typeof(string));
+		level = (int)info.GetValue("CharacterLevel", typeof(int));
+		currentXP = (int)info.GetValue("CharacterXP", typeof(int));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void GetObjectData(SerializationInfo info, StreamingContext context)
+	{
+		info.AddValue("CharacterName", name);
+		info.AddValue("CharacterLevel", level);
+		info.AddValue("CharacterXP", currentXP);
 	}
 }
